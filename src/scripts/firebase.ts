55d4a5b9b568585074bd7cfca2bd5d26931ab3fd
Martin <slackgt@bigpond.com>
2026-01-23 09:34:28 +1100

    23012026-5

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -1,18 +1,18 @@
 // src/lib/firebase.ts
 import admin from "firebase-admin";
 // src/lib/firebase.ts
-console.log("Checking Firebase ID:", import.meta.env.FIREBASE_PROJECT_ID);
+console.log("Checking Firebase ID:", process.env.FIREBASE_PROJECT_ID);
 
-if (!import.meta.env.FIREBASE_PRIVATE_KEY) {
+if (!process.env.FIREBASE_PRIVATE_KEY) {
   throw new Error("FIREBASE_PRIVATE_KEY is missing from .env");
 }
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: import.meta.env.FIREBASE_PROJECT_ID,
-      clientEmail: import.meta.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: import.meta.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
     }),
   });
 }
