1fd8b341cdfe8e8f79ee2a8f8f40caca1c19dc9a
Martin <slackgt@bigpond.com>
2026-01-23 09:51:23 +1100

    23012026-6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify/functions";
 import preact from "@astrojs/preact";
 export default defineConfig({
   devToolbar: {
