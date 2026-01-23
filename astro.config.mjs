c9bf1ed905bc80199d0fffbf2fa569812f2935f2
Martin <slackgt@bigpond.com>
2026-01-23 16:33:20 +1100

    23012026-9

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,26 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-// import netlify from "@astrojs/netlify";
 import preact from "@astrojs/preact";
+// import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
+
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
+
   trailingSlash: "always",
   site: "https://themaverick.net.au",
   integrations: [sitemap(), preact()],
+
   markdown: {
     extendDefaultPlugins: true,
   },
+
+  // adapter: netlify(),
   output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   // adapter: netlify(),
 });
--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -1,8 +1,12 @@
 // src/lib/firebase.ts
 import admin from "firebase-admin";
 // src/lib/firebase.ts
+// console.log("Checking Firebase ID:", import.meta.env.FIREBASE_PROJECT_ID);
 console.log("Checking Firebase ID:", process.env.FIREBASE_PROJECT_ID);
 
+// if (!import.meta.env.FIREBASE_PRIVATE_KEY) {
+//   throw new Error("FIREBASE_PRIVATE_KEY is missing from .env");
+// }
 if (!process.env.FIREBASE_PRIVATE_KEY) {
   throw new Error("FIREBASE_PRIVATE_KEY is missing from .env");
 }
@@ -16,6 +20,15 @@ if (!admin.apps.length) {
     }),
   });
 }
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert({
+//       projectId: import.meta.env.FIREBASE_PROJECT_ID,
+//       clientEmail: import.meta.env.FIREBASE_CLIENT_EMAIL,
+//       privateKey: import.meta.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+//     }),
+//   });
+// }
 
 export const db = admin.firestore();
 // Export admin so other files can see it
