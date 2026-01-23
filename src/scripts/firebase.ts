e78e5644331b8155de82fcc2d04e3c886c1a1100
Martin <slackgt@bigpond.com>
2026-01-24 08:36:03 +1100

    24012026-1

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -1,34 +1,34 @@
 // src/lib/firebase.ts
 import admin from "firebase-admin";
 // src/lib/firebase.ts
-// console.log("Checking Firebase ID:", import.meta.env.FIREBASE_PROJECT_ID);
-console.log("Checking Firebase ID:", process.env.FIREBASE_PROJECT_ID);
+console.log("Checking Firebase ID:", import.meta.env.FIREBASE_PROJECT_ID);
+// console.log("Checking Firebase ID:", process.env.FIREBASE_PROJECT_ID);
 
-// if (!import.meta.env.FIREBASE_PRIVATE_KEY) {
-//   throw new Error("FIREBASE_PRIVATE_KEY is missing from .env");
-// }
-if (!process.env.FIREBASE_PRIVATE_KEY) {
+if (!import.meta.env.FIREBASE_PRIVATE_KEY) {
   throw new Error("FIREBASE_PRIVATE_KEY is missing from .env");
 }
+// if (!process.env.FIREBASE_PRIVATE_KEY) {
+//   throw new Error("FIREBASE_PRIVATE_KEY is missing from .env");
+// }
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    }),
-  });
-}
 // if (!admin.apps.length) {
 //   admin.initializeApp({
 //     credential: admin.credential.cert({
-//       projectId: import.meta.env.FIREBASE_PROJECT_ID,
-//       clientEmail: import.meta.env.FIREBASE_CLIENT_EMAIL,
-//       privateKey: import.meta.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+//       projectId: process.env.FIREBASE_PROJECT_ID,
+//       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 //     }),
 //   });
 // }
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: import.meta.env.FIREBASE_PROJECT_ID,
+      clientEmail: import.meta.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: import.meta.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
+}
 
 export const db = admin.firestore();
 // Export admin so other files can see it
