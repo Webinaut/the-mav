abb7b69ae754b700e91c93b4ad5ff3dc48ae0f7d
Martin <slackgt@bigpond.com>
2026-07-14 09:14:52 +1000

    14072026-1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 // import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify";
+import mdx from "@astrojs/mdx";
 // import node from "@astrojs/node";
 
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   },
   trailingSlash: "always",
   site: "https://themaverick.net.au",
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
   // markdown: {
   //   extendDefaultPlugins: true,
   // },
